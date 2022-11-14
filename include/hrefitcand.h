0e4ae784b203c27a53086cad47f01161f46f0d5a
KinFit <waleed.a.esmail@gmail.com>
2022-11-14 12:57:21 +0100

    add another track parameterization pxpypz_xyz

--- include/hrefitcand.h
+++ include/hrefitcand.h
@@ -3,13 +3,17 @@
 
 // ROOT includes
 #include <TMath.h>
+#include <TVector3.h>
 #include <TMatrixD.h>
 #include <TLorentzVector.h>
+
+// framework includes
+#include "hparticletool.h"
 
 class HRefitCand : public TLorentzVector
 {
 
-ClassDef(HRefitCand, 1);
+    ClassDef(HRefitCand, 1);
 
 private:
     TLorentzVector *cand;
@@ -18,16 +22,17 @@ private:
     TMatrixD fCov;
 
 public:
-    HRefitCand(TLorentzVector* cand, Double_t R, Double_t Z);
-    HRefitCand(); //Does that work like this? See https://os.mbed.com/users/fpucher/code/HIM0Board/wiki/Vererbung-in-C%2B%2B
+    HRefitCand(TLorentzVector *cand, Double_t R, Double_t Z);
+    HRefitCand(TLorentzVector *cand, Double_t X, Double_t Y, Double_t Z);
+    HRefitCand(); // Does that work like this? See https://os.mbed.com/users/fpucher/code/HIM0Board/wiki/Vererbung-in-C%2B%2B
     ~HRefitCand(){};
-    void setMomentum(Double_t val){ fMomentum = val; }
+    void setMomentum(Double_t val) { fMomentum = val; }
     void setTheta(Double_t val) { fTheta = val; }
     void setPhi(Double_t val) { fPhi = val; }
     void setR(Double_t val) { fR = val; }
     void setZ(Double_t val) { fZ = val; }
     void setCovariance(const TMatrixD &cov);
-    void setPid(Int_t val) {fPid = val; }
+    void setPid(Int_t val) { fPid = val; }
 
     Double_t getMomentum() const { return fMomentum; }
     Double_t getTheta() const { return fTheta; }
--- source/hrefitcand.cxx
+++ source/hrefitcand.cxx
@@ -1,9 +1,28 @@
 // ROOT includes
 #include "hrefitcand.h"
 
-HRefitCand::HRefitCand(TLorentzVector* cand, Double_t R, Double_t Z)
+HRefitCand::HRefitCand(TLorentzVector *cand, Double_t R, Double_t Z)
     : TLorentzVector(*cand), cand(cand), fMomentum(cand->P()), fTheta(cand->Theta()), fPhi(cand->Phi()), fR(R), fZ(Z)
 {
+    fPid = -1;
+}
+
+HRefitCand::HRefitCand(TLorentzVector *cand, Double_t X, Double_t Y, Double_t Z)
+    : TLorentzVector(*cand), cand(cand), fMomentum(cand->P()), fTheta(cand->Theta()), fPhi(cand->Phi())
+{
+    Double_t deg2rad = TMath::DegToRad();
+
+    TVector3 base(X, Y, Z);
+    TVector3 beam_base, beam_dir;
+    TVector3 dir(TMath::Sin(fTheta) * TMath::Cos(fPhi),
+                 TMath::Sin(fTheta) * TMath::Sin(fPhi),
+                 TMath::Cos(fTheta));
+
+    HParticleTool::calcSegVector(1, 0, 0, 0, beam_base, beam_dir);
+    TVector3 POCA = HParticleTool::calculatePointOfClosestApproach(base, dir, beam_base, beam_dir);
+    fR = base.Y() * TMath::Cos(fPhi) - base.X() * TMath::Sin(fPhi);
+    fZ = POCA.Z();
+
     fPid = -1;
 }
 
@@ -16,21 +35,41 @@ HRefitCand::HRefitCand()
     fPid = -1;
 }
 
-void HRefitCand::setCovariance(const TMatrixD& cov)
+void HRefitCand::setCovariance(const TMatrixD &cov)
 {
+    // HADES default track parametrization
     // 0 = 1/p
     // 1 = theta
     // 2 = phi
     // 3 = R
     // 4 = z
-    fCov.ResizeTo(5, 5);
-    fCov = cov;
+
+    // An alternative parametrization
+    // 0 = px
+    // 1 = py
+    // 2 = pz
+    // 3 = x (vertex x position)
+    // 4 = y (vertex y position)
+    // 5 = z (vertex z position)
+
+    if (cov.GetNoElements() == 5 * 5)
+    {
+        fCov.ResizeTo(5, 5);
+        fCov = cov;
+    }
+
+    else if (cov.GetNoElements() == 6 * 6)
+    {
+        // TO-DO: do the error propagtion calculations
+    }
 }
 
-void HRefitCand::reset() {
-    *(TLorentzVector*)this = *cand;
+void HRefitCand::reset()
+{
+    *(TLorentzVector *)this = *cand;
 }
 
-void HRefitCand::update() {
-    *((TLorentzVector*)cand) = *this;
+void HRefitCand::update()
+{
+    *((TLorentzVector *)cand) = *this;
 }
